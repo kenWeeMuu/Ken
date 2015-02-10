0a8108accc769e00706cc629b8158934cd0fd5f4
KenWeeMuu <Ken@weemuu.com>
2015-02-10 15:49:18 -0800

    !!

--- Ken.DataStore/DataCaching/Program.cs
+++ Ken.DataStore/DataCaching/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace DataCaching
 {
@@ -10,6 +7,7 @@ namespace DataCaching
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
         }
     }
 }
